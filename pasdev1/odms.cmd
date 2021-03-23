c3e9bc7cae1b2ddcc39372bb6a7e38e5cfd2ba99
Lars Brinkhoff <lars@nocrew.org>
2021-03-23 14:02:03 +0000

    Files extracted from 169263.tape

---
+++ pasdev1/odms.cmd
@@ -0,0 +1,8 @@
+ODMS.SRL
+/s MMOVLM[31024,320210]
+MMDSEG[31024,320210]
+ mmodms,mmblds,mmbldp,mmdbop,mmdbpr,mmprnt,mmpack,mmmsym
+mmtcor,mmmpub,rename
+ /define:hstrt.:#65000
+/s rdlib[,320156],paslib[,320155]
+ODMS/SSAVE/G

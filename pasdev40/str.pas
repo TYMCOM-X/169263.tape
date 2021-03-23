a0a2e71f9978039bd2eda6f0b2f856e78cbac9dc
Lars Brinkhoff <lars@nocrew.org>
2021-03-23 19:51:58 +0000

    Files extracted from 169263.tape

---
+++ pasdev40/str.pas
@@ -0,0 +1,15 @@
+program str;
+
+var
+  string_var : string[10];
+
+  procedure junk (trash : packed array [1..4] of char);
+
+  begin
+    assert (trash = '4567');
+  end;
+
+begin
+  string_var := '1234567890';
+  junk (substr (string_var, 3, 4));
+end.
